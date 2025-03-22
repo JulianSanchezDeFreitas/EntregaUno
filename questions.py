b71adc35a835469e1b8d39c8936233d7caba5048
Julian Lihué Sanchez de Freitas <julihue.sanchez03@gmail.com>
2025-03-22 18:45:57 -0300

    modificacion: forma de acceder a las listas

--- questions.py
+++ questions.py
@@ -23,22 +23,23 @@ answers = [
 # Índice de la respuesta correcta para cada pregunta, el el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
 puntaje_total = 0
-# El usuario deberá contestar 3 preguntas
-for _ in range(3):
-# Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
+# Se crea la lista con 3 tuplas, en la que cada una tiene su pregunta sus posibles respuestas y el indice de la respuesta valida
+questions_to_ask = random.choices(list(zip(questions, answers, correct_answers_index)), k=3)
+# Se crean las tuplas con las preguntas, respuestas e indices de las respuestas correctas en base a la variable questions_to_ask
+for (preguntas, respuestas, indices) in (questions_to_ask):
+
 # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+    print(preguntas)
+    for i, answer in enumerate(respuestas):
         print(f"{i + 1}. {answer}")
 # El usuario tiene 2 intentos para responder correctamente
     for intento in range(2):
         user_answer = (input("Respuesta: "))
 # Verifico si es valida
-        if user_answer.isdigit() and (0 < int(user_answer) <= len(answers)):
+        if user_answer.isdigit() and (0 < int(user_answer) <= len(answers[0])):
             user_answer = int(user_answer)-1
 # Se verifica si la respuesta es correcta
-            if user_answer == correct_answers_index[question_index]:
+            if user_answer == indices:
                 print("¡Correcto!")
                 puntaje_total += 1
                 break
@@ -51,7 +52,7 @@ for _ in range(3):
 # Si el usuario no responde correctamente después de 2 intentos,
 # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index] [correct_answers_index[question_index]])
+        print(respuestas[indices])
 # Se imprime un blanco al final de la pregunta
     print()
 print(" el puntaje final fue: ",puntaje_total )
